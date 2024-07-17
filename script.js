ca692849efb3cb6efeb837490e08a86805db274c
Luiz Leal <143563882+cainhurst87@users.noreply.github.com>
2024-07-16 21:23:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const rColor = document.getElementById("red");
+    const gColor = document.getElementById("green");
+    const bColor = document.getElementById("blue");
+    const oColor = document.getElementById("opacity");
+
+    const sliders = document.querySelectorAll(".container-slider input[type='range']");
+    const copyBtn = document.getElementById("copy-btn");
+    const result = document.querySelector(".container-result input");
+
+    let generateColor = () => {
+        let rColorValue = rColor.value;
+        let gColorValue = gColor.value;
+        let bColorValue = bColor.value;
+        let oColorValue = oColor.value;
+
+        let finalColor = `rgba(${rColorValue}, ${gColorValue}, ${bColorValue}, ${oColorValue})`;
+        result.value = finalColor;
+        document.body.style.backgroundColor = finalColor;
+    };
+
+    let copyColorCode = () => {
+        result.select();
+        document.execCommand("copy");
+        copyBtn.innerText = "Copied";
+        setTimeout(() => {
+            copyBtn.innerText = "Copy Color Code";
+        }, 1000);
+    };
+
+    sliders.forEach((inp) => {
+        inp.addEventListener("input", generateColor);
+    });
+
+    window.addEventListener("load", generateColor);
+    copyBtn.addEventListener("click", copyColorCode);
+
+    setInterval(() => {
+        console.log(`Red: ${rColor.value}, Green: ${gColor.value}, Blue: ${bColor.value}`);
+    }, 1000);
+});
